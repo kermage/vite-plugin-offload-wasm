e6725abf9f3a06a8494d7b43edbc8841ef4e8f24
kermage <genealyson.torcende@gmail.com>
2024-06-24 20:46:06 +0800

    functional bit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# vite-plugin-offload-wasm
+# Vite Plugin: Offload WASM
+
+> Serve WASM from a CDN or any remote location, bypassing the local server entirely
+
+## Usage
+
+`pnpm add -D vite-plugin-offload-wasm`
+
+```ts
+import offloadWasm from 'vite-plugin-offload-wasm';
+
+export default defineConfig({
+	plugins: [
+		offloadWasm({
+			'path.to.wasm': 'https://example.com/with/file.wasm',
+			'@another/test.wasm': 'https://unpkg.com/@another/test.wasm',
+		}),
+	],
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,65 @@
+import { extname, sep } from 'path';
 import type { Plugin } from 'vite';
 
-export default function offloadWasm(): Plugin {
+export default function offloadWasm(list: Record<string, string>): Plugin {
+	function definedReplacement(id: string) {
+		const pathParts = id.split(sep);
+
+		for (let i = 1; i <= pathParts.length; i++) {
+			const partialPath = pathParts.slice(-i).join(sep);
+
+			if (list.hasOwnProperty(partialPath)) {
+				return list[partialPath];
+			}
+		}
+
+		return '';
+	}
+
+	function replacerData(moduleCode: string, replacement: string) {
+		const ASSET_PATTERN = /__VITE_ASSET__.*__/;
+
+		return {
+			pattern: new RegExp(moduleCode.replace('$', `\\$`).replace(ASSET_PATTERN, '[^\\;]+')),
+			replacement: moduleCode?.replace(ASSET_PATTERN, replacement),
+		};
+	}
+
+	function handleReplacements(code: string, data: ReturnType<typeof replacerData>) {
+		return code.replace(data.pattern, data.replacement);
+	}
+
 	return {
 		name: 'vite-plugin-offload-wasm',
+		enforce: 'post',
+		apply: 'build',
+
+		renderChunk(code, chunk) {
+			if ('object' !== typeof list || 0 === Object.values(list).length) {
+				return code;
+			}
+
+			Object.keys(chunk.modules).forEach((id) => {
+				if ('.wasm?url' !== extname(id)) {
+					return;
+				}
+
+				const moduleCode = chunk.modules[id].code;
+
+				if (!moduleCode) {
+					return;
+				}
+
+				const replacement = definedReplacement(id.replace('?url', ''));
+
+				if (!replacement) {
+					return;
+				}
+
+				code = handleReplacements(code, replacerData(moduleCode, replacement));
+			});
+
+			return code;
+		},
 	};
 }
