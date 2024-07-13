706d53a6de4440f49aef914ba3806486bfc71353
kermage <genealyson.torcende@gmail.com>
2024-07-14 00:47:22 +0800

    update requirements

--- README.md
+++ README.md
@@ -4,13 +4,15 @@
 
 ## Usage
 
-`pnpm add -D vite-plugin-offload-wasm`
+`pnpm add -D vite-plugin-wasm vite-plugin-offload-wasm`
 
 ```ts
+import wasm from 'vite-plugin-wasm';
 import offloadWasm from 'vite-plugin-offload-wasm';
 
 export default defineConfig({
   plugins: [
+    wasm(),
     offloadWasm({
       'path.to.wasm': 'https://example.com/with/file.wasm',
       '@another/test.wasm': 'https://unpkg.com/@another/test.wasm',
--- tests/vite.config.ts
+++ tests/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
-import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 import offloadWasm from '../src/index';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		target: 'esnext',
+	},
 	plugins: [
 		wasm(),
-		topLevelAwait(),
 		offloadWasm({
 			'tests/test.wasm': 'https://cdn.jsdelivr.net/gh/kermage/vite-plugin-offload-wasm/tests/test.wasm',
 		}),
